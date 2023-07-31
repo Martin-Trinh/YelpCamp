f173b30a05ebc61270d006dc7a13529ae29eecd5
Dinh Quang Trinh <trinhdin@fit.cvut.cz>
2023-07-31 06:51:59 +0200

    Add style and data

--- seeds/index.js
+++ seeds/index.js
@@ -19,11 +19,17 @@ const seedDb = async () => {
     await Campground.deleteMany({})
     for( let i = 0; i < 50; i++){
         const index = Math.floor(Math.random() *1000)
+        const randomPrice = Math.floor(Math.random() * 30) + 10
         const camp = await new Campground({
             location: `${cities[index].city}, ${cities[index].state}`,
-            title: `${randomPicker(descriptors)} ${randomPicker(places)}`
+            title: `${randomPicker(descriptors)} ${randomPicker(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil voluptatibus vitae cum similique harum placeat earum vero doloribus, dolorem doloremque, iusto natus dolore sequi atque. Error amet quo at nihil.',
+            price: randomPrice
         })
-        await camp  .save()
+        await camp.save()
     }
 }
-seedDb()
+seedDb().then(() => {
+    mongoose.connection.close()
+})
